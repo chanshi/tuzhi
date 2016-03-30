afde8ba3c89673a007b98d7b783f9f028eeebed1
sky_macbook <jiese@chanshi.me>
2016-03-30 20:00:48 +0800

    更新了 mockery

--- vender/Tuzhi/web/Application.php
+++ vender/Tuzhi/web/Application.php
@@ -5,3 +5,23 @@
  * Date: 16/3/27
  * Time: 11:44
  */
+
+namespace tuzhi\web;
+
+
+class Application extends \tuzhi\base\Application {
+
+    public $request;
+
+    public $route;
+
+    public $respond;
+
+    public function __construct( $config = [] ){
+        parent::__construct( $config );
+    }
+
+    public function run(){
+
+    }
+}
